1944afe0b4917efa7cb3ba2e19bcda1b4c2d713c
stanislavorlov <stasorlov21@gmail.com>
2025-03-15 20:37:57 +0200

    web socket services

---
+++ chess-game/chess-game.v3/web/client/chess-app/src/app/services/websocket.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  private socket$: WebSocketSubject<any>;
+
+  constructor() {
+    this.socket$ = webSocket('ws://your-websocket-url');
+  }
+
+  // Send a message to the server
+  sendMessage(message: any) {
+    this.socket$.next(message);
+  }
+
+  // Receive messages from the server
+  getMessages(): Observable<any> {
+    return this.socket$.asObservable();
+  }
+
+  // Close the WebSocket connection
+  closeConnection() {
+    this.socket$.complete();
+  }
+}

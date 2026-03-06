76b8460720ce5a6f33f86212c73c787e3199f2a4
stanislavorlov <stasorlov21@gmail.com>
2026-03-06 21:57:11 +0200

    move schema and model into different folders

--- backend/authapp/src/models/Player.ts
+++ backend/authapp/src/models/Player.ts
@@ -1,5 +1,4 @@
-import mongoose, { Schema, Document } from 'mongoose';
-import bcrypt from 'bcryptjs';
+import { Document } from 'mongoose';
 
 export interface IPlayer extends Document {
     username: string;
@@ -10,54 +9,3 @@ export interface IPlayer extends Document {
     createdAt: Date;
     matchPassword(enteredPassword: string): Promise<boolean>;
 }
-
-const PlayerSchema: Schema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [
-            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-            'Please add a valid email'
-        ]
-    },
-    passwordHash: {
-        type: String,
-        required: true,
-        select: false // Do not return password hash by default
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    level: {
-        type: Number,
-        required: true,
-    },
-    country: {
-        type: String,
-        required: true,
-    }
-});
-
-// Method to verify passwords
-PlayerSchema.methods.matchPassword = async function (enteredPassword: string) {
-    return await bcrypt.compare(enteredPassword, this.passwordHash);
-};
-
-// Pre-save hook to hash password if it was modified
-PlayerSchema.pre('save', async function (this: any) {
-    if (!this.isModified('passwordHash')) {
-        return;
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
-});
-
-export const Player = mongoose.model<IPlayer>('Player', PlayerSchema);
---
+++ backend/authapp/src/schema/player.schema.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs';
+import { IPlayer } from "../models/Player";
+
+const PlayerSchema: Schema<IPlayer> = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+        select: false // Do not return password hash by default
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    level: {
+        type: Number,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    }
+});
+
+// Method to verify passwords
+PlayerSchema.methods.matchPassword = async function (enteredPassword: string) {
+    return await bcrypt.compare(enteredPassword, this.passwordHash);
+};
+
+// Pre-save hook to hash password if it was modified
+PlayerSchema.pre('save', async function (this: any) {
+    if (!this.isModified('passwordHash')) {
+        return;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+});
+
+export const Player = mongoose.model<IPlayer>('Player', PlayerSchema);
